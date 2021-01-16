3f14312d16a683bfc534a7aeba6505d4bd44dcae
Erik van der Bas <erik@basedonline.nl>
2021-01-16 14:47:16 +0100

    init commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+(function ($) {
+  console.log("hi");
+  $(".something").on("click", function () {
+    //
+  });
+})(jQuery); // Fully reference jQuery after this point.
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const DependencyExtractionWebpackPlugin = require("@wordpress/dependency-extraction-webpack-plugin");
+const WebpackManifestPlugin = require("webpack-manifest-plugin");
+module.exports = {
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new DependencyExtractionWebpackPlugin({
+      outputFormat: "json",
+    }),
+    new WebpackManifestPlugin({
+      publicPath: "",
+    }),
+  ],
+};
